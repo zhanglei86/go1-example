8b41731b31f59a80eb198e6562b7fdc21a29cdfd
zhanglei <zhanglei863@crnet.com.hk>
2019-03-08 18:53:16 +0800

    kafka client

---
+++ kafka_client/producer_async1.go
@@ -0,0 +1,61 @@
+package main
+// goroutines方式
+
+import (
+  "github.com/Shopify/sarama"
+  "log"
+  "os"
+  "sync"
+)
+
+func main() {
+  config := sarama.NewConfig()
+  config.Producer.Return.Successes = true
+  producer, err := sarama.NewAsyncProducer([]string{"localhost:9092"}, config)
+  if err != nil {
+      panic(err)
+  }
+
+  // Trap SIGINT to trigger a graceful shutdown.
+  signals := make(chan os.Signal, 1)
+  //signal.Notify(signals, os.Interrupt)
+
+  var (
+      wg sync.WaitGroup
+      enqueued, successes, errors int
+  )
+
+  wg.Add(1)
+  go func() {
+      defer wg.Done()
+      for range producer.Successes() {
+          successes++
+      }
+  }()
+
+  wg.Add(1)
+  go func() {
+      defer wg.Done()
+      for err := range producer.Errors() {
+          log.Println(err)
+          errors++
+      }
+  }()
+
+  ProducerLoop:
+  for {
+      message := &sarama.ProducerMessage{Topic: "my_topic", Value: sarama.StringEncoder("testing123")}
+      select {
+      case producer.Input() <- message:
+          enqueued++
+
+      case <-signals:
+          producer.AsyncClose() // Trigger a shutdown of the producer.
+          break ProducerLoop
+      }
+  }
+
+  wg.Wait()
+
+  log.Printf("Successfully produced: %d; errors: %d\n", successes, errors)
+}
